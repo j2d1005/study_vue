a1b719f278a8b6e88940b977b4387b84391fc2ce
Sam.Hong <sam.hong3@kakaocorp.com>
2020-09-02 19:07:26 +0900

    기본 폼 전송

---
+++ vue-form/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <form v-on:submit.prevent="submitForm">
+        <div>
+            <label for="username">id: </label>
+            <input type="text" id="username" v-model="username">
+        </div>
+        <div>
+            <label for="password">password: </label>
+            <input type="password" id="password" v-model="password">
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data: function() {
+        return {
+            username: '',
+            password: '',
+        }
+    },
+    methods: {
+        submitForm: function() {
+            console.log(this.username, this.password)
+            let url = 'https://jsonplaceholder.typicode.com/users/';
+            let data = {
+                username: this.username,
+                password: this.password,
+            }
+            axios.post(url, data)
+                .then(function(res) {
+                    console.log(res)
+                })
+                .catch(function(err) {
+                    console.log(err)
+                })
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
