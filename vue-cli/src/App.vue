dfb684510c72a313187621f9fc1b43ed9b8cf8d4
Sam.Hong <sam.hong3@kakaocorp.com>
2020-09-02 18:33:06 +0900

    vue-cli로 프로젝트 생성 / 기본 구조와 사용

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+<!--        <AppHeader v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></AppHeader>-->
+        <AppHeader
+            v-bind:propsdata="str"
+            v-on:renew="renewStr"
+        ></AppHeader>
+    </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+
+export default {
+    // vue 인스턴스에서 선언할 때와 다르게 객체를 만들어서 리턴해야함
+    data: function() {
+        return{
+            str: 'Header',
+        }
+    },
+    components: {
+        'AppHeader': AppHeader
+    },
+    methods: {
+        renewStr: function(val) {
+            this.str = val;
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <h1>{{ propsdata }}</h1>
+        <input type="text" v-model="val" v-on:keyup.enter="sendEvent(val)">
+        <button v-on:click="sendEvent(val)">send</button>
+    </header>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            val: '',
+        }
+    },
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function(val) {
+            this.$emit('renew', val);
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
