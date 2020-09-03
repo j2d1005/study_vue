e54fb92811b22706cebf74dae6e199dad427b8c1
Sam.Hong <sam.hong3@kakaocorp.com>
2020-09-03 18:03:27 +0900

    vue transition & animation 맛보기 (공식문서)

--- age-of-vue-02/vue-todo/src/components/TodoList.vue
+++ age-of-vue-02/vue-todo/src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ul>
+        <transition-group name="list" tag="ul">
             <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
                 <span class="checkBtn">
                     <i class="fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
@@ -10,7 +10,7 @@
                     <i class="fas fa-trash-alt"></i>
                 </span>
             </li>
-        </ul>
+        </transition-group>
     </div>
 </template>
 
@@ -61,6 +61,16 @@ export default {
     .textCompletd {
         text-decoration: line-through;
         color: #b3adad;
+    }
+
+
+    /*리스트 아이템 트렌지션*/
+    .list-enter-active, .list-leave-active {
+        transition: all 1s;
+    }
+    .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+        opacity: 0;
+        transform: translateY(30px);
     }
 
 </style>
