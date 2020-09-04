faeec8f8a3a56a35f3cb27b12b9f1c518cb39142
Sam.Hong <sam.hong3@kakaocorp.com>
2020-09-04 16:54:32 +0900

    모듈화된 store에서 state 가져오기 // 아직 잘 안됨

--- age-of-vue-02/vue-todo/src/components/TodoHeader.vue
+++ age-of-vue-02/vue-todo/src/components/TodoHeader.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+<!--        <h1>{{ this.$store.state.headerText }}</h1>-->
         <h1>{{ this.headerText }}</h1>
     </div>
 </template>
@@ -9,8 +10,23 @@ import { mapState } from 'vuex';
 export default {
     name: "TodoHeader",
     computed: {
-        ...mapState(['headerText'])
+        ...mapState({
+            headerText: state => state.todoApp.headerText
+        })
     }
+
+    // 모듈화된 스토어에서 mapState 쓰는법 예제 ..
+    // https://kdydesign.github.io/2019/04/06/vuejs-vuex-helper/
+    // computed: {
+    //     ... mapState({
+    //         bookList: state => state.A.B.C.Book.list
+    //     })
+    // },
+    // methods: {
+    //     ...mapActions([
+    //         'A/B/C/Book/setList'
+    //     ])
+    // }
 }
 </script>
 
