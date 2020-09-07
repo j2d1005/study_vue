b7b9391429b204aab533348ea81891cef0fcb034
Sam.Hong <sam.hong3@kakaocorp.com>
2020-09-07 11:59:03 +0900

    news-app init & router 기본 설정

---
+++ age-of-vue-03/vue-news/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <ToolBar />
+      <router-view />
+  </div>
+</template>
+
+<script>
+import ToolBar from "./components/ToolBar";
+
+export default {
+
+    components: {
+        ToolBar,
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ age-of-vue-03/vue-news/vue.config.js
@@ -0,0 +1,4 @@
+// package 파일에 설정해놓은 eslint 끄는 법
+module.exports = {
+    lintOnSave: false
+}
