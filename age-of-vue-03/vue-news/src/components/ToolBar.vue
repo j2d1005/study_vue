a132e134d63a34b4169deb9fd0db10830a434bb0
Sam.Hong <sam.hong3@kakaocorp.com>
2020-09-07 12:21:38 +0900

    routes 추가 및 mode 설정

--- age-of-vue-03/vue-news/src/components/ToolBar.vue
+++ age-of-vue-03/vue-news/src/components/ToolBar.vue
@@ -1,17 +1,23 @@
 <template>
-    <div>
-        <router-link to="/news">News</router-link>
-        <router-link to="/ask">Ask</router-link>
+    <div class="header">
+        <router-link to="/news">News</router-link> |
+        <router-link to="/ask">Ask</router-link> |
         <router-link to="/jobs">Jobs</router-link>
     </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
 
 <style scoped>
-
+    .header {
+        color: white;
+        background-color: #42b883;
+        display: flex;
+        padding: 8px;
+    }
+    .header a {
+        color: inherit;
+    }
+    .header a.router-link-active {
+        color:#354953;
+    }
 </style>
--- age-of-vue-03/vue-news/src/router/index.js
+++ age-of-vue-03/vue-news/src/router/index.js
@@ -3,11 +3,14 @@ import VueRouter from 'vue-router';
 import AskView from '../views/AskView.vue';
 import NewsView from '../views/NewsView';
 import JobsView from '../views/JobsView';
+import ItemView from '../views/ItemView';
+import UserView from '../views/UserView';
 
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+    mode: 'history',
     routes: [
         {
             path: '/',
@@ -24,6 +27,14 @@ export const router = new VueRouter({
         {
             path: '/jobs',
             component: JobsView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+        {
+            path: '/item',
+            component: ItemView,
         }
     ]
 });
