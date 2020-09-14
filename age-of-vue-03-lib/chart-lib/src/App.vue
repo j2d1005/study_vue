20e377924b032be77ebef918833ad3cd47f9587f
Sam.Hong3 <sam.hong3@kakaocorp.com>
2020-09-14 16:50:09 +0900

    chart 라이브러리 실습

--- age-of-vue-03-lib/chart-lib/src/App.vue
+++ age-of-vue-03-lib/chart-lib/src/App.vue
@@ -2,14 +2,17 @@
     <div>
         <h1>Chart.js</h1>
         <BarChart></BarChart>
+        <LineChart></LineChart>
     </div>
 </template>
 
 <script>
 import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
 export default {
     components: {
         BarChart,
+        LineChart
     }
 }
 </script>
---
+++ age-of-vue-03-lib/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <canvas id="lineChart"></canvas>
+    </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+
+export default {
+    // mounted 안에 선언한 이유는 ctx = doucment. ~~~ 부분에 document로 선언해 놓은 부분때문이다. 돔이 만들어 진 후에 선언이 되어야 해서 그렇다
+    mounted() {
+        var ctx = document.getElementById('lineChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45]
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
