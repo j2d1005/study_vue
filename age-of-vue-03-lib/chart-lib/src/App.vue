62ffbd4dfe8eef231576047c4d8646d85d2fe7a5
Sam.Hong3 <sam.hong3@kakaocorp.com>
2020-09-14 16:08:51 +0900

    vue - 외부 라이브러리 실습

---
+++ age-of-vue-03-lib/chart-lib/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Chart.js</h1>
+        <BarChart></BarChart>
+    </div>
+</template>
+
+<script>
+import BarChart from "./components/BarChart";
+export default {
+    components: {
+        BarChart,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
