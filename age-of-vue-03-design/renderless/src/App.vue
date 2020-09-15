59f612996089922701fcba8dbf3e8fe0aacc7b70
Sam.Hong3 <sam.hong3@kakaocorp.com>
2020-09-15 18:43:27 +0900

    renderless컴포넌트 - render함수와 scopedSlots

--- age-of-vue-03-design/renderless/src/App.vue
+++ age-of-vue-03-design/renderless/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
-      <!-- ... -->
+      <!-- 하위 컴포넌트에서 render함수 안에 $scopedSlots로 올린 데이터를 slot-scope로 받을 수 있다. -->
+      <!-- slot-scope가 선언된 태그 내에서만 속성에 접근가능 -->
       <div slot-scope="{response, loading}">
         <div v-if="!loading">
           <p>name : {{response.name}}</p>
--- age-of-vue-03-design/renderless/src/main.js
+++ age-of-vue-03-design/renderless/src/main.js
@@ -5,18 +5,19 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  // 1
+  // render 함수 작성법
+  // 1. 기본문법
   render: function(createElement) {
     return createElement(App);
   },
-  // 2
+  // 2. h는 버츄얼돔을 지칭하는 약자라고 보면 됨
   render: function(h) {
     return h(App);
   },
-  // 3
+  // 3.
   render: (h) => {
     return h(App);
   },
-  // 4
+  // 4.
   render: h => h(App),
 }).$mount('#app')
