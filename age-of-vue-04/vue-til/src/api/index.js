491a311b58978cba9951f219660057d1a4aaaa1f
Sam.Hong3 <sam.hong3@kakaocorp.com>
2020-09-17 15:57:11 +0900

    axios api 공통화 설정

--- age-of-vue-04/vue-til/src/api/index.js
+++ age-of-vue-04/vue-til/src/api/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+const instance = axios.create({
+	baseURL: 'http://localhost:3000/',
+});
+
 function registerUser(userData) {
-	const url = 'http://localhost:3000/signup';
-	return axios.post(url, userData);
+	// const url = 'http://localhost:3000/signup';
+	// return axios.post(url, userData);
+
+	return instance.post('signup', userData);
 	// promise이기 때문에 return을 해줘야 그 이후 비동기동작을 수행할 수 있다.
 }
 
