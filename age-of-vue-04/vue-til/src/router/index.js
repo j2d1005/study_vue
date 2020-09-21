8684e4d310d011db61983641bb51032c6a13a32e
Sam.Hong3 <sam.hong3@kakaocorp.com>
2020-09-21 18:39:35 +0900

    네비게이션가드를 사용해서 인증되지 않은 사용자의 페이지 접근 제어

--- age-of-vue-04/vue-til/src/router/index.js
+++ age-of-vue-04/vue-til/src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '../store';
 
 // import LoginPage from '../views/LoginPage';
 // import SignupPage from '../views/SignupPage';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
 	mode: 'history',
 	routes: [
 		{
@@ -30,14 +31,18 @@ export default new VueRouter({
 		{
 			path: '/main',
 			component: () => import('../views/MainPage'),
+			// meta 속성을 이용해서 라우터에 부가정보를 부여할 수 있다.
+			meta: { auth: true },
 		},
 		{
 			path: '/add',
 			component: () => import('../views/PostAddPage'),
+			meta: { auth: true },
 		},
 		{
 			path: '/post/:id',
 			component: () => import('../views/PostEditPage'),
+			meta: { auth: true },
 		},
 		{
 			// 위에 url을 제외한 모든 url에 대해 설정
@@ -46,3 +51,25 @@ export default new VueRouter({
 		},
 	],
 });
+
+// 라우터 네비게이션가드 기본 문법
+router.beforeEach((to, from, next) => {
+	if (to.meta.auth && !store.getters.isLogin) {
+		console.log('인증이 필요합니다.');
+		next('/login');
+		// 인증이 필요한 페이지이고 사용자가 로그인을 하지 않은 상태라면 /login으로 redirect
+		return;
+	}
+	console.log(to);
+	next();
+	// next()를 호출해야지만 to 페이지로 이동할 수 있다.
+});
+
+export default router;
+
+// 라우터 네비게이션가드를 사용할 때
+// export default new VueRouter({});를
+
+// const router = new VueRouter({});
+// export default router;
+// 로 변경해줘야 한다.
