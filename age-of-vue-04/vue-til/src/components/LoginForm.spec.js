62fe9d35c38161075a12c9bdec93967208abaaba
Sam.Hong3 <sam.hong3@kakaocorp.com>
2020-09-22 17:45:30 +0900

    vue컴포넌트 jest로 테스트하기

--- age-of-vue-04/vue-til/src/components/LoginForm.spec.js
+++ age-of-vue-04/vue-til/src/components/LoginForm.spec.js
@@ -1,14 +1,10 @@
-import { sum } from './math';
+import Vue from 'vue';
+import LoginForm from './LoginForm';
 
-// describe('sum 함수 테스트 코드');
-describe('math.js', () => {
-	test('10 + 20 = 30', () => {
-		const result = sum(10, 20);
-		expect(result).toBe(30);
-		// expect(result).not.toBe(15);
+describe('LoginForm.vue', () => {
+	test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
+		const instance = new Vue(LoginForm).$mount();
+		// console.log(instance);
+		expect(instance.username).toBe('');
 	});
 });
-
-// describe - 연관된 테스트 케이스를 그릅화하는 API
-// test - 하나의 테스트 케이스를 검증하는 API
-// expect - expect(result).toBe(30); result가 30이 맞는지 비교 ?
